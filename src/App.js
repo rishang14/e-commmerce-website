e612a45b6bb2d37a09b32fe471c1e397aff48a9b
Rishang <rishangkumar9693@gmail.com>
2023-05-14 00:05:03 +0530

    added react router and add some changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import Directory from "./components/directory/directory.component";
 
 const App =()=>{ 
@@ -6,31 +5,31 @@ const App =()=>{
   const cateogries=[ 
     {
       id: 1,
-      title: "hats",
+      title: "Hats",
       imageUrl: "https://i.ibb.co/cvpntL1/hats.png"
     },
     {
       id: 2,
-      title: "jackets",
+      title: "Jackets",
       imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"
     },
     {
       id: 3,
-      title:"sneakers",
+      title:"Sneakers",
       imageUrl:"https://i.ibb.co/0jqHpnp/sneakers.png"
     },
     {
       id: 4,
-      title:"womens",
+      title:"Womens",
       imageUrl:"https://i.ibb.co/GCCdy8t/womens.png"
     },
     {
       id: 5,
-      title:"mens",
+      title:"Mens",
       imageUrl:"https://i.ibb.co/R70vBrQ/men.png"
     }
   ] 
-  //  hello 
+ 
    return(
      <Directory cateogries={cateogries}/>
    );
