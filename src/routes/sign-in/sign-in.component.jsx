cd73dc0a8566b1ebdd094c5ec29866b31a728fc7
Rishang <rishangkumar9693@gmail.com>
2023-05-22 13:20:45 +0530

    added new feature

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,25 +1,23 @@
-// import {signInWithGooglePopup,createUserDocumnetFromAuth} from "../../utils/firebase/firebase.utils";
-import  SignUpForm from "../../components/sign-up-form/sign-up-form.component"; 
- 
- const SignIn =()=>{
-//     const logGoogleUser= async () =>{  
-//         try{
-//         const {user} =await  signInWithGooglePopup(); 
-//          const userDocRef=  await createUserDocumnetFromAuth(user);
-//     } catch(err){ 
-//         alert("error resolved");
-//     } 
-// } 
-
-    return(
-        <div> 
-            <h1>Sign In page </h1> 
-            {/* <button onClick={logGoogleUser}>Sing in with Google</button>  */}
-          <SignUpForm/> 
-        
-        
-        </div>
-    )
-}
- 
-export default SignIn;
+import {
+    signInWithGooglePopup,
+    createUserDocumentFromAuth,
+  } from '../../utils/firebase/firebase.utils';
+  
+  import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+  
+  const SignIn = () => {
+    const logGoogleUser = async () => {
+      const { user } = await signInWithGooglePopup();
+      const userDocRef = await createUserDocumentFromAuth(user);
+    };
+  
+    return (
+      <div>
+        <h1>Sign In Page</h1>
+        <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+        <SignUpForm />
+      </div>
+    );
+  };
+  
+  export default SignIn;
