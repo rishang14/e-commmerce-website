ff2a164da90c4abadae3b0fed08631aef038a44d
Rishang <rishangkumar9693@gmail.com>
2023-05-23 11:20:06 +0530

    added styping to  login page

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 
-// import FormInput from '../form-input/form-input.component';
-// import Button from '../button/button.component';
+import FormInput from '../form-input/form-input.component';
+ import Button from '../button/button.component';
 
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
-// import './sign-up-form.styles.scss';
+import './sign-up-form.styles.scss';
 
 const defaultFormFields = {
   displayName: '',
@@ -54,24 +54,52 @@ const SignUpForm = () => {
     const { name, value } = event.target;
 
     setFormFields({ ...formFields, [name]: value });
-  };
+  }; 
 
-  return (
-    <div>
-        <h1>Sign up with your email  and password</h1>
-  <form onSubmit={handleSubmit}>
-      <label htmlFor="displayName">Dispaly Name</label>
-      <input type="text" required value={displayName} onChange={handleChange} name='displayName'/>
-      <label htmlFor="email">Email</label>
-      <input type="email" required value={email} onChange={handleChange}  name='email'/>
-      <label htmlFor="password">Password</label>
-      <input type="password" required value={password} onChange={handleChange}  name='password'/>
-      <label htmlFor="password">Confirm Password</label>
-      <input type="password" required value={confirmPassword} onChange={handleChange}  name='confirmPassword'/>
-      <button type='submit'>Sign Up</button>
-  </form>
-  </div>
-);
+return(
+  <div className='sign-up-container'>
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label='Display Name'
+          type='text'
+          required
+          onChange={handleChange}
+          name='displayName'
+          value={displayName}
+        />
+
+        <FormInput
+          label='Email'
+          type='email'
+          required
+          onChange={handleChange}
+          name='email'
+          value={email}
+        />
+
+        <FormInput
+          label='Password'
+          type='password'
+          required
+          onChange={handleChange}
+          name='password'
+          value={password}
+        />
+
+        <FormInput
+          label='Confirm Password'
+          type='password'
+          required
+          onChange={handleChange}
+          name='confirmPassword'
+          value={confirmPassword}
+        />
+        <Button  type='submit'>Sign Up</Button>
+      </form>
+    </div>
+  );
 };
 
 export default SignUpForm;
